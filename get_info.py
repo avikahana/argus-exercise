c23c957edbdaf5bc407a0f9396b6e7c043fb87e4
Avi Kahana <avi.kahana@gmail.com>
2023-07-07 01:45:43 +0300

    Update get_info.py
    testing path

--- get_info.py
+++ get_info.py
@@ -16,8 +16,7 @@ os_version = platform.release()
 data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
 print(data)
 # File name
-# file_path = "info.txt"
-file_path = os.path.join(os.getcwd(), 'file.txt')
+file_path = "/var/jenkins_home/workspace/avikahana-pipeline/info.txt"
 
 # Open the file in write mode
 with open(file_path, "w") as file:
