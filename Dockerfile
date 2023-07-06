87f3f6d875da45b080e81e878b6382f2d72805bb
Avi Kahana <avi.kahana@gmail.com>
2023-07-06 21:25:26 +0300

    testing
    testing

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,10 @@ FROM python:3.9-slim
 WORKDIR /app
 
 # Copy the python script into the container
-COPY requirements.txt /app/
-COPY get_info.py /app/
-
-# Install dependencies
-RUN pip install --no-cache-dir -r requirements.txt
+COPY get_info.py .
 
 # Define the command to run your Python script
-CMD ["python", "get_info.py"]
+ENTRYPOINT ["python", "get_info.py"]
 
+# Expose the output directory as a volume to host
+VOLUME /app/output
--- get_info.py
+++ get_info.py
@@ -1,24 +1,39 @@
-import datetime
+# import datetime
+# import platform
+
+# # Get the current date and time
+# current_date = datetime.datetime.now().strftime("%d-%m-%Y")
+# current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+# # Get the operating system name
+# os_name = platform.system()
+
+# # Get the operating system version
+# os_version = platform.release()
+
+# # Formated data
+# data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
+# print(data)
+# # File name
+# file_path = "info.txt"
+
+# # Open the file in write mode
+# with open(file_path, "w") as file:
+#     file.write(data)
+
+
 import platform
+import subprocess
 
-# Get the current date and time
-current_date = datetime.datetime.now().strftime("%d-%m-%Y")
-current_time = datetime.datetime.now().strftime("%H:%M:%S")
-
-# Get the operating system name
+# Get operating system information
 os_name = platform.system()
+os_version = platform.version()
 
-# Get the operating system version
-os_version = platform.release()
+# Get current date and time
+date_output = subprocess.check_output(["date"]).decode().strip()
 
-# Formated data
-data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
-print(data)
-# File name
-file_path = "info.txt"
+# Print Current date & time
+print("Current Date and Time:", date_output)
 
-# Open the file in write mode
-with open(file_path, "w") as file:
-    file.write(data)
-
-
+# Print OS name & version
+print("Operating System:", os_name, "| OS Version:", platform.release())
