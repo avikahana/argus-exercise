20fcc7483968e1fe7ae13e0f1839a738b7967fd5
Avi Kahana <avikahana@users.noreply.github.com>
2023-07-06 10:33:26 +0300

    Added files

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official python image as the base image
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the python script into the container
+COPY requirements.txt /app/
+COPY get_info.py /app/
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Define the command to run your Python script
+CMD ["python", "get_info.py"]
+
+# Expose the output directory as a volume to host
+VOLUME /app
---
+++ get_info.py
@@ -0,0 +1,24 @@
+import datetime
+import platform
+
+# Get the current date and time
+current_date = datetime.datetime.now().strftime("%d-%m-%Y")
+current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+# Get the operating system name
+os_name = platform.system()
+
+# Get the operating system version
+os_version = platform.release()
+
+# Formated data
+data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
+
+# File name
+file_path = "info.txt"
+
+# Open the file in write mode
+with open(file_path, "w") as file:
+    file.write(data)
+
+
