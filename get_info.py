6944c507397bea82ae61aea8e7d9d6202f068857
Avi Kahana <avi.kahana@gmail.com>
2023-07-06 21:35:53 +0300

    Update get_info.py
    testing

--- get_info.py
+++ get_info.py
@@ -1,39 +1,22 @@
-# import datetime
-# import platform
-
-# # Get the current date and time
-# current_date = datetime.datetime.now().strftime("%d-%m-%Y")
-# current_time = datetime.datetime.now().strftime("%H:%M:%S")
-
-# # Get the operating system name
-# os_name = platform.system()
-
-# # Get the operating system version
-# os_version = platform.release()
-
-# # Formated data
-# data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
-# print(data)
-# # File name
-# file_path = "info.txt"
-
-# # Open the file in write mode
-# with open(file_path, "w") as file:
-#     file.write(data)
-
-
+import datetime
 import platform
-import subprocess
 
-# Get operating system information
+# Get the current date and time
+current_date = datetime.datetime.now().strftime("%d-%m-%Y")
+current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+# Get the operating system name
 os_name = platform.system()
-os_version = platform.version()
 
-# Get current date and time
-date_output = subprocess.check_output(["date"]).decode().strip()
+# Get the operating system version
+os_version = platform.release()
 
-# Print Current date & time
-print("Current Date and Time:", date_output)
+# Formated data
+data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
+print(data)
+# File name
+file_path = "/app/info.txt"
 
-# Print OS name & version
-print("Operating System:", os_name, "| OS Version:", platform.release())
+# Open the file in write mode
+with open(file_path, "w") as file:
+    file.write(data)
