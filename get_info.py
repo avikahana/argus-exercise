d137d060a2c8bf120b23d108e4511670f3917010
Avi Kahana <avi.kahana@gmail.com>
2023-07-07 22:36:25 +0300

    Update get_info.py
    test

--- get_info.py
+++ get_info.py
@@ -16,7 +16,7 @@ os_version = platform.release()
 data = f"date: {current_date}, time: {current_time}, os_name: {os_name}, os_version: {os_version}"
 print(data)
 # File name
-file_path = "info.txt"
+file_path = "/app/info.txt"
 print(os.getcwd())
 # Open the file in write mode
 with open(file_path, "w") as file:
